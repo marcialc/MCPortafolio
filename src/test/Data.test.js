84fa48060b6d2902137a65404fcd7176499d7073
Marcial <marcialandres@me.com>
2019-09-03 00:59:54 -0400

    added testing with jest files for the app.js and for the data being used in the webapp

---
+++ src/test/Data.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Data from '../Data';
+
+test('My info', () => {
+    const name  = renderer.create(Data.name);
+    let tree = name.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    const headline  = renderer.create(Data.headline);
+    tree = headline.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    const aboutme  = renderer.create(Data.aboutme);
+    tree = aboutme.toJSON();
+    expect(tree).toMatchSnapshot();
+});
