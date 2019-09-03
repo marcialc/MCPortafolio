ad2b0dc32f8b164c29e3b43bac90b72deba88b32
Marcial <marcialandres@me.com>
2019-09-03 02:56:41 -0400

    trial

--- src/screens/Experience.js
+++ src/screens/Experience.js
@@ -7,6 +7,7 @@ export default class Experience extends Component {
       <section id="experience">
         {/* Education Section */}
         <div className="row education">
+        
           <div className="three columns header-col">
             <h1>
               <span>education</span>
