012c308803995136f717e3948f33499186b8ce82
Marcial <marcialandres@me.com>
2019-09-03 01:28:06 -0400

    trial

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 export default class Home extends Component {
   render() {
     let Data = this.props.Data;
+    
     return (
       <React.Fragment>
         <header id="home">
