51be19e9030825de2ae03b006edc63bc31f02de8
Marcial <marcialandres@me.com>
2019-09-03 14:01:21 -0400

    test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from "../App";
-import ReactDOM from 'react-dom';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
