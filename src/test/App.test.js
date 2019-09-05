b75da0c37f0d114b84d11e57e407b6a5dd573565
Marcial <marcialandres@me.com>
2019-09-05 17:56:00 -0400

    update

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,6 +3,7 @@ import App from "../App";
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
+
 configure({adapter: new Adapter()});
 describe('React component test with Enzyme', () => {
   it('renders without crashing', () => {
